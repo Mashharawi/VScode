554605db2c05669b0068d105481f124aa20a9bc7
Mashharawi <moh6390@hotmail.com>
2019-07-17 12:07:22 +0300

    Regexp

---
+++ Re.py
@@ -0,0 +1,8 @@
+import re
+pattern = '[A-Z0-9]'
+test_string = input("Enter Text:     ")
+result = re.match(pattern, test_string)
+if result:
+  print("Search successful.")
+else:
+  print("Search unsuccessful.")	
